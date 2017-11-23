f822d176291c7b093156cafe491892e8c7096204
Ori Newman <orinewman1@gmail.com>
2017-11-23 19:25:43 +0200

    change favicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import checkBalance from './check-balance';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
